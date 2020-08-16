36d85f443dd504d117f33c8f5bc04c0472ba645b
Will Lytle <wlytle89@gmail.com>
2020-08-16 20:39:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -109,6 +109,16 @@ game = {
       blocks: 1,
       slam_dunks: 0
       },
+      player_name: "Ben Gordon",
+      number: 8,
+      shoe: 15,
+      points: 33,
+      rebounds: 3,
+      assists: 2,
+      steals: 1,
+      blocks: 1,
+      slam_dunks: 0
+      }
      ]
   }
 }
