68f9f7f086e19a24ed6648d886f04e41a0aead43
Will Lytle <wlytle89@gmail.com>
2020-08-16 20:38:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -88,6 +88,27 @@ game = {
       blocks: 15,
       slam_dunks: 10
       },
+      {
+      player_name: "DeSagna Diop",
+      number: 2,
+      shoe: 14,
+      points: 24,
+      rebounds: 12,
+      assists: 12,
+      steals: 4,
+      blocks: 5,
+      slam_dunks: 5
+      },
+      player_name: "Ben Gordon",
+      number: 8,
+      shoe: 15,
+      points: 33,
+      rebounds: 3,
+      assists: 2,
+      steals: 1,
+      blocks: 1,
+      slam_dunks: 0
+      },
      ]
   }
 }
