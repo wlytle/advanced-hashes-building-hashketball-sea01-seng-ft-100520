ccc851fafabe18002756ab975fc14b1bb5048632
Will Lytle <wlytle89@gmail.com>
2020-08-16 20:21:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,17 @@ game = {
       steals: 3,
       blocks: 1,
       slam_dunks: 1
-     }
+     },
+     :alan_anderson => {
+      player_name: "Alan Anderson",
+      number: 0,
+      shoe: 16,
+      points: 22,
+      rebounds: 12,
+      assists: 12,
+      steals: 3,
+      blocks: 1,
+      slam_dunks: 1
      ]
   },
   away: {
