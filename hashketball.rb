f35fd457b11bd86740d451e94a31467f16355052
Will Lytle <wlytle89@gmail.com>
2020-08-16 19:52:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,13 +6,14 @@ game = {
    players: [
      {
       player_name: "Alan Anderson",
-      number: "0",
-      points: "",
-      rebounds: "",
-      assists: "",
-      steals: "",
-      blocks: "",
-      slam_dunks: ""
+      number: 0,
+      shoe: 16,
+      points: 22,
+      rebounds: 12,
+      assists: 12,
+      steals: 3,
+      blocks: 1,
+      slam_dunks: 1
      }
      ]
   },
@@ -23,6 +24,7 @@ game = {
      {
       player_name: "",
       number: "",
+      shoe: ,
       points: "",
       rebounds: "",
       assists: "",
