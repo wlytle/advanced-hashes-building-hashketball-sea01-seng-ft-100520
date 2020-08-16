c16c6416ed1f57ad27802e305d011d6c38692ff7
Will Lytle <wlytle89@gmail.com>
2020-08-16 20:36:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -49,6 +49,17 @@ game = {
       blocks: 8,
       slam_dunks: 5
       },
+      {
+      player_name: "Jason Terry",
+      number: 31,
+      shoe: 15,
+      points: 19,
+      rebounds: 2,
+      assists: 2,
+      steals: 4,
+      blocks: 11,
+      slam_dunks: 1
+      },
      ]
   },
   away: {
