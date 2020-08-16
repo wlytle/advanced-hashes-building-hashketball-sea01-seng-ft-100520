a8d7ff19b9e6eed2ff322d612de607b9d8efe698
Will Lytle <wlytle89@gmail.com>
2020-08-16 20:01:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ game = {
    team_name: "Brooklyn Nets",
    colors: ["Black", "White"],
    players: [
-    "Alan Anderson" => {
+    alan_anderson: {
       player_name: "Alan Anderson",
       number: 0,
       shoe: 16,
