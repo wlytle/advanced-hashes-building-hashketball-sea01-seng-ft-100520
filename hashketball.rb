c248fe78ff6053c87f46ebc1a4ea2d76157420ec
Will Lytle <wlytle89@gmail.com>
2020-08-16 20:35:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -26,7 +26,29 @@ game = {
       steals: 12,
       blocks: 12,
       slam_dunks: 7
-      }
+      },
+      {
+      player_name: "Brook Lopez",
+      number: 11,
+      shoe: 17,
+      points: 17,
+      rebounds: 19,
+      assists: 10,
+      steals: 3,
+      blocks: 1,
+      slam_dunks: 15
+      },
+      {
+      player_name: "Mason Plumlee",
+      number: 1,
+      shoe: 19,
+      points: 26,
+      rebounds: 11,
+      assists: 6,
+      steals: 3,
+      blocks: 8,
+      slam_dunks: 5
+      },
      ]
   },
   away: {
