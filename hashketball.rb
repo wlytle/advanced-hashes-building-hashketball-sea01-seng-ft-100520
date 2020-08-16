6fdfad5fc6e5334be6915fb5477a122c2a5884dc
Will Lytle <wlytle89@gmail.com>
2020-08-16 19:45:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,22 @@
 def game_hash
 game = {
   home: {
-    
+   team_name: "",
+   colors: [],
+   players: [
+     {
+      player_name: "",
+      numer: "",
+      points: "",
+      rebounds: "",
+      assists: "",
+      steals: "",
+      blocks: "",
+      slam_dunks: ""
+     }
+     ]
   },
-  away: {
-    
-  }
+  away: {}
 }
 
 end
