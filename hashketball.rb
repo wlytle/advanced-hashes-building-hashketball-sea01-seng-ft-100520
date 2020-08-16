d0d1d8ef72926d4157874a2d121b8f9760ceca27
Will Lytle <wlytle89@gmail.com>
2020-08-16 20:22:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,16 +16,16 @@ game = {
       blocks: 1,
       slam_dunks: 1
      },
-     :alan_anderson => {
-      player_name: "Alan Anderson",
-      number: 0,
-      shoe: 16,
-      points: 22,
+     reggie_evans: {
+      player_name: "Reggie Evans",
+      number: 30,
+      shoe: 14,
+      points: 12,
       rebounds: 12,
       assists: 12,
-      steals: 3,
-      blocks: 1,
-      slam_dunks: 1
+      steals: 12,
+      blocks: 12,
+      slam_dunks: 7
      ]
   },
   away: {
